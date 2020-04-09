dc6c3f8cc1d1775e46b3f67feae3da4baec3955b
Kristoffer Henneberg Duus <kris494p@student.eadania.dk>
2020-04-09 19:43:53 +0200

    string added to quicksort and insertion

--- InsertionSort/Program.cs
+++ InsertionSort/Program.cs
@@ -10,30 +10,60 @@ namespace InsertionSort
     {
         static void Main(string[] args)
         {
-            int[] cardarray = new int[] { 5, 4, 3, 10, 13, 12, 11, 0 };
-            Console.WriteLine("Insertion Sort");
-            CardSort(cardarray);
-        }
-        public static void CardSort(int[] cardarray)
-        {
-            
-            int value;
-            int pointer;
-            for (int i = 1; i < cardarray.Length; i++)
+            #region insertion int
+            //int[] cardarray = new int[] { 5, 4, 3, 10, 13, 12, 11, 0 };
+            //Console.WriteLine("Insertion Sort");
+            //CardSort(cardarray);
+            #endregion
+            #region Insertion string
+            foreach (var name in stringSort(new string[] { "Bo" , "Julie" , "Torsten" , "Arne" , "Willy" , "Camilla" , "Ida" }))
             {
-                value = cardarray[i];
-                pointer = i;
-                while (pointer > 0 && value < cardarray[pointer - 1])
+                Console.WriteLine(name);
+            }
+            #endregion
+            Console.ReadLine();
+        }
+        #region insertion int metoder
+        //public static void CardSort(int[] cardarray)
+        //{
+
+        //    int value;
+        //    int pointer;
+        //    for (int i = 1; i < cardarray.Length; i++)
+        //    {
+        //        value = cardarray[i];
+        //        pointer = i;
+        //        while (pointer > 0 && value < cardarray[pointer - 1])
+        //        {
+        //            cardarray[pointer] = cardarray[pointer - 1];
+        //            pointer = pointer - 1;
+        //        }
+        //        cardarray[pointer] = value;
+        //    }
+        //    foreach (int card in cardarray)
+        //    {
+        //        Console.WriteLine(card.ToString());
+        //    }
+        //}
+        #endregion
+        #region Insertion string metode
+
+        public static string[] stringSort(string[] names)
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                string value = names[i];
+                int pointer = i;
+
+                while (pointer > 0 && string.Compare(value,names[pointer - 1])<0)
                 {
-                    cardarray[pointer] = cardarray[pointer - 1];
+                    names[pointer] = names[pointer - 1];
                     pointer = pointer - 1;
                 }
-                cardarray[pointer] = value;
+                names[pointer] = value;
             }
-            foreach (int card in cardarray)
-            {
-                Console.WriteLine(card.ToString());
-            }
+            return names;
         }
+        #endregion
     }
 }
--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -10,46 +10,105 @@ namespace QuickSort
     {
         static void Main(string[] args)
         {
-            List<int> quicklist = new List<int>();
-            quicklist.Add(5);
-            quicklist.Add(-5);
-            quicklist.Add(50);
-            quicklist.Add(20);
-            quicklist.Add(35);
-            quicklist.Add(200);
-            quicklist.Add(1);
-            quicklist.Add(-100);
-            quicklist.Add(0);
-            quicklist.Add(42);
+            #region quicksort string
+            List<string> stringQuickList = new List<string>();
+            stringQuickList.Add("Bo");
+            stringQuickList.Add("Julie");
+            stringQuickList.Add("Torsten");
+            stringQuickList.Add("Arne");
+            stringQuickList.Add("Willy");
+            stringQuickList.Add("Camilla");
+            stringQuickList.Add("Ida");
 
-            Console.WriteLine("QuickSort");
-            List<int> list = QuickSort(quicklist);
-            for (int i = 0; i < quicklist.Count; i++)
+            Console.WriteLine("QuickSort String");
+
+            List<string> slist = StringQuickSort(stringQuickList);
+            for (int i = 0; i < stringQuickList.Count; i++)
             {
-                Console.WriteLine(list[i]);
+                Console.WriteLine(slist[i]);
             }
+            #endregion
+            #region quicksort int
+            //List<int> quicklist = new List<int>();
+            //quicklist.Add(5);
+            //quicklist.Add(-5);
+            //quicklist.Add(50);
+            //quicklist.Add(20);
+            //quicklist.Add(35);
+            //quicklist.Add(200);
+            //quicklist.Add(1);
+            //quicklist.Add(-100);
+            //quicklist.Add(0);
+            //quicklist.Add(42);
+
+            //Console.WriteLine("QuickSort int");
+            //List<int> list = QuickSort(quicklist);
+            //for (int i = 0; i < quicklist.Count; i++)
+            //{
+            //    Console.WriteLine(list[i]);
+            //}
+            #endregion
+            Console.ReadLine();
         }
-        public static List<int> QuickSort(List<int> quicksortList)
+        #region quicksort int metoder
+        //public static List<int> QuickSort(List<int> quicksortList)
+        //{
+        //    return QuickSort(quicksortList, 0, quicksortList.Count - 1);
+        //}
+
+        //private static List<int> QuickSort(List<int> list, int startIndex, int endIndex)
+        //{
+        //    if (list.Count <= 1)
+        //    {
+        //        return list;
+        //    }
+
+        //    int pivot = list[startIndex];
+
+        //    List<int> tempList = new List<int>();
+        //    List<int> before = new List<int>();
+        //    List<int> after = new List<int>();
+
+        //    for (int i = 1; i < list.Count; i++)
+        //    {
+        //        if (list[i] < pivot)
+        //        {
+        //            before.Add(list[i]);
+        //        }
+        //        else
+        //        {
+        //            after.Add(list[i]);
+        //        }
+        //    }
+
+        //    tempList.AddRange(QuickSort(before));
+        //    tempList.Add(pivot);
+        //    tempList.AddRange(QuickSort(after));
+        //    return tempList;
+        //}
+        #endregion
+        #region quicksortString metoder
+        public static List<string> StringQuickSort(List<string> quicksortList)
         {
-            return QuickSort(quicksortList, 0, quicksortList.Count - 1);
+            return StringQuickSort(quicksortList, 0, quicksortList.Count - 1);
         }
 
-        private static List<int> QuickSort(List<int> list, int startIndex, int endIndex)
+        private static List<string> StringQuickSort(List<string> list, int startIndex, int endIndex)
         {
             if (list.Count <= 1)
             {
                 return list;
             }
 
-            int pivot = list[startIndex];
+            string pivot = list[startIndex];
 
-            List<int> tempList = new List<int>();
-            List<int> before = new List<int>();
-            List<int> after = new List<int>();
+            List<string> tempList = new List<string>();
+            List<string> before = new List<string>();
+            List<string> after = new List<string>();
 
             for (int i = 1; i < list.Count; i++)
             {
-                if (list[i] < pivot)
+                if (string.Compare(list[i], pivot)<0)
                 {
                     before.Add(list[i]);
                 }
@@ -59,10 +118,11 @@ namespace QuickSort
                 }
             }
 
-            tempList.AddRange(QuickSort(before));
+            tempList.AddRange(StringQuickSort(before));
             tempList.Add(pivot);
-            tempList.AddRange(QuickSort(after));
+            tempList.AddRange(StringQuickSort(after));
             return tempList;
         }
+        #endregion
     }
 }
