54cfa49a69be913fd0e8f92144b64427c4a342fe
Kristoffer Henneberg Duus <kris494p@student.eadania.dk>
2020-04-24 02:18:42 +0200

    different oop added

---
+++ OOP6- DesignPatterns/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP6__DesignPatterns
+{
+    public class Book
+    {
+        private Library library;
+        private bool borrowed;
+
+
+        public void Borrow()
+        {
+
+        }
+    }
+}
---
+++ OOP6- DesignPatterns/Library.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP6__DesignPatterns
+{
+    public class Library
+    {
+        private Book[] books;
+        private User[] user;
+        private Employee[] employee;
+
+        public void Borrow(User borrower,Book book)
+        {
+
+        }
+
+        public int Handin(User borrower)
+        {
+            return 0;
+        }
+    }
+}
